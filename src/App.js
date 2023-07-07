a599b85af2c8124b17363e3a4c6a32a5e9428966
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2023-07-07 16:42:58 -0400

    final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Portfolio from './components/Portfolio'
-
 import Skills from './components/Skills'
 import './App.scss'
 
